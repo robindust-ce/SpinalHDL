4676a027c6afa9b5ab36a01bffcf4c55d5c99548
robindust-ce <79113691+robindust-ce@users.noreply.github.com>
2022-04-14 12:46:43 +0200

    Typos in comments in WishboneDecoder.scala

--- lib/src/main/scala/spinal/lib/bus/wishbone/WishboneDecoder.scala
+++ lib/src/main/scala/spinal/lib/bus/wishbone/WishboneDecoder.scala
@@ -8,14 +8,14 @@ import scala.collection.Seq
 
 /** Factory for [[spinal.lib.bus.wishbone.WishboneDecoder]] instances. */
 object WishboneDecoder{
-  /** Create a istance of a wishbone decoder/multiplexer
+  /** Create a instance of a wishbone decoder/multiplexer
   * @param config it will use for configuring all the input/output wishbone port
   * @param decodings it will use for configuring the partial address decoder
   * @return a [[spinal.lib.bus.wishbone.WishboneDecoder]] instance
   */
   def apply(config: WishboneConfig, decodings: Seq[SizeMapping]) = new WishboneDecoder(config,decodings)
 
-  /** Create an istance of WishboneDecoder, and autocconect all input/outputs
+  /** Create an instance of WishboneDecoder, and autoconnect all inputs/outputs
     * @param master connect the input to this master interface, the [[spinal.lib.bus.wishbone.Wishbone.config]] will be used for all input/output
     * @param slaves connect the ouput to the slaves, with the correct address
     * @return a [[spinal.lib.bus.wishbone.WishboneDecoder]] instance with all the input and output connected automatically
@@ -38,7 +38,7 @@ class WishboneDecoder(config : WishboneConfig, decodings : Seq[SizeMapping]) ext
     val outputs = Vec(master(Wishbone(config)),decodings.size)
   }
 
-  //permanently drive some slave iunput signal to save on logic usage
+  //permanently drive some slave input signal to save on logic usage
   io.outputs.map{ out =>
     out.STB       := io.input.STB
     out.DAT_MOSI  := io.input.DAT_MOSI
@@ -56,9 +56,9 @@ class WishboneDecoder(config : WishboneConfig, decodings : Seq[SizeMapping]) ext
   // the selector will use the decodings list to select the right slave based on the address line
   val selector = Vec(decodings.map(_.hit(io.input.ADR) && io.input.CYC))
 
-  // Generate the CYC sygnal for the selected slave
+  // Generate the CYC signal for the selected slave
   (io.outputs.map(_.CYC), selector).zipped.foreach(_ := _)
-  //Implementing the multiplexer logic, it thakes the one Hot bit vector/bit array as input
+  //Implementing the multiplexer logic, it takes the one Hot bit vector/bit array as input
   io.input.ACK      := MuxOH(selector, Vec(io.outputs.map(_.ACK)))
   io.input.DAT_MISO := MuxOH(selector, Vec(io.outputs.map(_.DAT_MISO)))
 
